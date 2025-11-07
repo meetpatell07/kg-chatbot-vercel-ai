bbb3219b5f3bca46d3be743e3e8649d99a07ff73
Meet Patel <meet@skeon.tech>
2025-11-07 00:29:06 -0500

    Updated config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    '@chroma-core/default-embed',
+    '@huggingface/transformers',
+    'onnxruntime-node',
+    'sharp',
+  ],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Externalize native dependencies for server-side
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@chroma-core/default-embed': 'commonjs @chroma-core/default-embed',
+        '@huggingface/transformers': 'commonjs @huggingface/transformers',
+        'onnxruntime-node': 'commonjs onnxruntime-node',
+        'sharp': 'commonjs sharp',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
